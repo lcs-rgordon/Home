90a9899d21b228ee353eaf7dc42b6315410a20f6
Russell Gordon <rgordon@lcs.on.ca>
2024-01-11 14:21:13 -0500

    Created a basic linear gradient.

--- Home/ContentView.swift
+++ Home/ContentView.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored property
+    let purpleToRed = Gradient(colors: [Color.purple, Color.red])
+    
+    // MARK: Computed property
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 100)
-            Text("Hello, world!")
-                .font(.largeTitle)
+        ZStack {
+            
+            // Bottom layer
+            LinearGradient(
+                gradient: purpleToRed,
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            
+            // Top layer
+            VStack {
+                Image(systemName: "globe")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+                Text("Hello, world!")
+                    .font(.largeTitle)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
